6374286d93be113bb850275571fffcd56ac135a3
Leonard <adrianinformatica1920@gmail.com>
2024-10-29 13:19:32 +0100

    comentado el security.yaml

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,10 +12,14 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
+        // Redirigir si el usuario ya está autenticado
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_page'); // Cambia 'app_page' por la ruta adecuada
+        }
 
-        // last username entered by the user
+        // Obtener el error de autenticación si existe
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // Último nombre de usuario introducido
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', [
@@ -23,6 +27,7 @@ class SecurityController extends AbstractController
             'error' => $error,
         ]);
     }
+
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
